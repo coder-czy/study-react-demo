66e82b52d443d83a15de00e69bf35e3faa2655d5
yangzi <1131696774@qq.com>
2023-08-07 15:40:21 +0800

    项目准备

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import "./App.css"
-import List from "./components/List"
-import Count from "./components/Count"
-import Comment from "./components/Comment"
 
 function App () {
-  const list = ["Vue", "React", "Angular"]
-  let sendMsg = (msg) => {
-    console.log(msg)
-  }
   return (
     <div className="App">
-      <List list={list} sendMsg={sendMsg}></List>
-      <Count></Count>
-      <Comment></Comment>
+      123
     </div>
   )
 }
